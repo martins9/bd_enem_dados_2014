2b8802f2b139fa9223f571cc95723f034b35ad9d
martins <saulomartins.martins@gmail.com>
2017-10-07 02:50:32 -0300

    Upload t_realiza

---
+++ t_realiza/t_realiza_2014.sh
@@ -0,0 +1,11 @@
+# Rodando o primeiro script python 
+python t_realiza_2014.py
+
+# Rodando o segundo script python
+python t_realiza_2014_1.py
+
+# Deletando o arquivo 
+rm t_realiza_1.csv
+
+# Fazendo o load para a respectiva tabela
+./load_tabela.sh
