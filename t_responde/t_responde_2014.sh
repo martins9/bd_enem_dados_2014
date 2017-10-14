d99da7ebd89102f53632e3fe99568c4680d6404d
martins <saulomartins.martins@gmail.com>
2017-10-14 18:45:07 -0300

    Novos reparos

--- t_responde/t_responde_2014.sh
+++ t_responde/t_responde_2014.sh
@@ -14,7 +14,7 @@ rm t_responde_2014_1.csv
 path1=$(pwd)
 
 # Fazendo split dos arquivos e colocando
-split -d -a6 -l 1000000 --additional-suffix=.csv t_responde_2014_2.csv $path1/t_responde_base/t_responde_2014_
+split -d -a6 -l 30000000 --additional-suffix=.csv t_responde_2014_2.csv $path1/t_responde_base/t_responde_2014_
 
 # Excluindo os arquivos de base
 rm t_responde_2014_2.csv
@@ -31,7 +31,7 @@ QTD_ARQ1=$((QTD_ARQ-1))
 for i in $(seq -f "%06g" 0 $QTD_ARQ1)
 do
     # psql -h localhost -d bd_enem_dados_local -U anacrl -c
-	time (psql -h localhost -d bd_enem_dados_local -U martins -c "\copy responde from $path1/t_responde_base/t_responde_2014_$i.csv
+	time (psql -h localhost -d bd_enem_dados -U anacrl -c "\copy responde from $path1/t_responde_base/t_responde_2014_$i.csv
 	        with delimiter as ',' NULL AS 'null' csv") &>> t_responde_log.txt
 done
 
