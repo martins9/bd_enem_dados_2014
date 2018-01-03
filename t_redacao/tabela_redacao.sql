163e7ac638c32c0e3dcf6e947a780d7c817d65eb
martins9 <saulomartins.martins@gmail.com>
2018-01-03 16:12:16 -0200

    Update tabela_redacao.sql

--- t_redacao/tabela_redacao.sql
+++ t_redacao/tabela_redacao.sql
@@ -1,1 +1,1 @@
-INSERT INTO public.redacao(sequencial_redacao, ano_redacao, tema) VALUES (1,2014,'Publicidade infantil em questão no Brasil');
+INSERT INTO redacao(sequencial_redacao, ano_redacao, tema) VALUES (1,2014,'Publicidade infantil em questão no Brasil');
