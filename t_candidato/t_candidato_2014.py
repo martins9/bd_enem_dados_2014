9a4282e53339ab9c7c9e6d2c624f658049f20643
martins9 <saulomartins.martins@gmail.com>
2021-08-23 00:05:54 -0300

    Update t_candidato_2014.py

--- t_candidato/t_candidato_2014.py
+++ t_candidato/t_candidato_2014.py
@@ -8,7 +8,7 @@ for linha in arquivoentrada:
 	texto = linha.split(",")
 
 
-	arquivosaida.writelines (texto[0]+","+texto[2]+","+texto[18]+","+texto[58]+","+texto[7]+",2,"+
+	arquivosaida.writelines (texto[0]+","+texto[2]+","+texto[18]+","+texto[54]+","+texto[7]+",2,"+
 		texto[17]+","+texto[23]+","+texto[26]+","+texto[22]+","+texto[25]+","+texto[27]+","+texto[1]+
 		","+texto[15]+","+texto[16]+","+texto[84]+","+texto[85]+","+texto[86]+","+
     	texto[87]+","+texto[88]+","+texto[89]+","+texto[78]+","+texto[83]+'\n')
