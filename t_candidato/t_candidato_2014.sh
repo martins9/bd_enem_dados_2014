c10d03846863a6d4454bcbd8bfb6140b2e73e107
martins9 <saulomartins.martins@gmail.com>
2021-11-10 00:11:20 -0300

    Update t_candidato_2014.sh
    Alteração dos comentários

--- t_candidato/t_candidato_2014.sh
+++ t_candidato/t_candidato_2014.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Pegando todos os dados para o candidato 
+# Extraindo todos os dados para o candidato 
 python t_candidato_2014.py
 
-# Colando null nos espacos ,, 
+# Substituindo ",," por ",null,"
 sed 's/,,/,null,/g' t_candidato_2014.csv > t_candidato_2014_2.csv
 
-# Excluindo o arquivo
+# Excluindo o arquivo gerado
 rm t_candidato_2014.csv
 
-# Colando null nos lugares de "." e "*"
+# Substituindo "." e "*" por ",null,"
 cat t_candidato_2014_2.csv | sed 's/,\.,/,null,/g' | sed 's/,\*,/,null,/g' > t_candidato_final.csv
 
-# Excluindo o arquivo
+# Excluindo o arquivo gerado
 rm t_candidato_2014_2.csv
 
-# Load para o table candidato
+# Realizando o load dos dados para a tabela Candidato
 ./load_tabela.sh
